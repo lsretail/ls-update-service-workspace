e6c69a8c32e3be6121033e247684dce01ae48052
Arnthor Magnusson <arnthor@lsretail.com>
2019-10-29 14:04:53 +0000

    Fix GoC installing error due to deprecation in code.exe

--- package/Package.psm1
+++ package/Package.psm1
@@ -4,8 +4,13 @@ $ErrorActionPreference = 'stop'
 function Install-Package()
 {
     $FilePath = (Get-Item (Join-Path $PSScriptRoot '*.vsix')).FullName
-    $Output = & code --install-extension $FilePath  | Out-String
-    if ($LASTEXITCODE -ne 0)
+
+    Write-Progress -Id 217 -Activity 'Installing extension' -Status "Installing" -PercentComplete 20
+    $Process = Start-Process -FilePath 'code' -ArgumentList @('--install-extension', $FilePath) -WindowStyle Hidden -PassThru
+    Write-Progress -Id 217 -Activity 'Installing extension' -Status "Done" -PercentComplete 80
+    $Process.WaitForExit()
+    
+    if ($Process.ExitCode -ne 0)
     {
         Write-Host "Was trying to install $FilePath"
         throw "Exit code was ${LASTEXITCODE}: $Output"
